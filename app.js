b3dfa4228d6cea9cf3591fd7e848101e8b7d525b
nhanlinh1478 <luongtrongnhan1082000@gmail.com>
2021-10-30 18:01:55 +0700

    update-cors

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var Router = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+var cors = require("cors");
 
+app.use(cors());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
